575d897424799c3f42808e0d7512d302a8a7539b
ryglu <rygielpiotr18@gmail.com>
2024-01-25 17:00:36 +0100

    Fixed styling.

--- ecommerce_vue/src/views/HomeView.vue
+++ ecommerce_vue/src/views/HomeView.vue
@@ -11,7 +11,7 @@
       </div>
     </section>
 
-    <div class="column is-multiline">
+    <div class="columns is-multiline">
       <div class="column is-12">
         <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
