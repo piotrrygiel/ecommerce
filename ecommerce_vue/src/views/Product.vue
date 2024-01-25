62e3a4e0e52b231086a33ed286af18043314410e
ryglu <rygielpiotr18@gmail.com>
2024-01-25 15:11:54 +0100

    Implement loading bar.

--- ecommerce_vue/src/views/Product.vue
+++ ecommerce_vue/src/views/Product.vue
@@ -46,11 +46,13 @@ export default {
     this.getProduct()
   },
   methods: {
-    getProduct() {
+    async getProduct() {
+      this.$store.commit('setIsLoading', true)
+
       const category_slug = this.$route.params.category_slug
       const product_slug = this.$route.params.product_slug
 
-      axios
+      await axios
           .get(`/api/v1/products/${category_slug}/${product_slug}/`)
           .then(response => {
             this.product = response.data
@@ -58,6 +60,8 @@ export default {
           .catch(error => {
             console.log(error)
           })
+
+      this.$store.commit('setIsLoading', false)
     },
     addToCart() {
       if (isNaN(this.quantity) || this.quantity < 1) {
