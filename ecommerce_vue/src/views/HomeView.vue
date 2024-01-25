e0a40b762628f079d58e82aa1fb4b765e8da583b
ryglu <rygielpiotr18@gmail.com>
2024-01-25 16:12:19 +0100

    Convert the product to a reusable component.

--- ecommerce_vue/src/views/HomeView.vue
+++ ecommerce_vue/src/views/HomeView.vue
@@ -16,21 +16,10 @@
         <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
 
-      <div
-          class="column is-3"
-          v-for="product in latestProducts"
-          v-bind:key="product.id">
-        <div class="box">
-          <figure class="image mb-4">
-            <img :src="product.get_thumbnail">
-          </figure>
-
-          <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-
-          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
-        </div>
-      </div>
+      <ProductBox
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+        v-bind:product="product" />
     </div>
   </div>
 </template>
@@ -38,14 +27,23 @@
 <script>
 import axios from 'axios'
 
+import ProductBox from '@/components/ProductBox.vue'
+import category from "@/views/Category.vue";
+
 export default {
   name: 'HomeView',
+  computed: {
+    category() {
+      return category
+    }
+  },
   data() {
     return {
       latestProducts: []
     }
   },
   components: {
+    ProductBox
   },
   mounted() {
     this.getLatestProducts()
@@ -66,11 +64,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-  .image {
-    margin-top: -1.25rem;
-    margin-left: -1.25rem;
-    margin-right: -1.25rem;
-  }
-</style>
