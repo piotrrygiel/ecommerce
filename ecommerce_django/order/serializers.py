d60eba094dd69df3687461229b1f1bb70fd31e2d
ryglu <rygielpiotr18@gmail.com>
2024-02-04 19:41:24 +0100

    Implement Stripe payment. Create serializers for orders, update urls, create checkout view in backend.

---
+++ ecommerce_django/order/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import Order, OrderItem
+from product.serializers import ProductSerializer
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    """Serializer for item in order."""
+    class Meta:
+        model = OrderItem
+        fields = (
+            "price",
+            "product",
+            "quantity",
+        )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    """Serializer for order."""
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "address",
+            "zipcode",
+            "place",
+            "phone",
+            "stripe_token",
+            "items",
+        )
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        order = Order.objects.create(**validated_data)
+
+        for item_data in items_data:
+            OrderItem.objects.create(order=order, **item_data)
+
+        return order
